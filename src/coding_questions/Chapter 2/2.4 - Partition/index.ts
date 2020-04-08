8257d1a37eb968fb88deb95876f27400d0eebcc9
Jake Perez <jacobperez@berkeley.edu>
2020-04-08 00:30:03 -0500

    Complete Partition coding question

---
+++ src/coding_questions/Chapter 2/2.4 - Partition/index.ts
@@ -0,0 +1,45 @@
+/**
+ * Partition: Write code to partition a linked list around a value x, such that all nodes less than x come
+ * before all nodes greater than or equal to x. If x is contained within the list, the values of x only need
+ * to be after the elements less than x (see below). The partition element x can appear anywhere in the
+ * "right partition"; it does not need to appear between the left and right partitions.
+ *
+ * EXAMPLE
+ *
+ * Input:
+ * 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition= 5]
+ *
+ * Output:
+ * 3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8
+ */
+
+import { LinkedList, Node } from '../../../data_structures';
+
+const partition = (value: number, list: LinkedList<any>): void => {
+  let ptr = list.head;
+  if (!ptr) {
+    return;
+  }
+  let startingPoint = null;
+  if (ptr.value === value) {
+    startingPoint = ptr;
+  } else {
+    while (ptr && ptr.value <= value) {
+      startingPoint = ptr;
+      ptr = ptr.next;
+    }
+  }
+
+  let newPtr = startingPoint!.next;
+  while (newPtr) {
+    if (newPtr.value < value) {
+      const temp = startingPoint!.value;
+      startingPoint!.value = newPtr.value;
+      newPtr.value = temp;
+      startingPoint = startingPoint!.next;
+    }
+    newPtr = newPtr.next;
+  }
+};
+
+export { partition };
